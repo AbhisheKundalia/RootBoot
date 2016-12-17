112bd75b13c7d4eac92afee894f7077dd0b5181c
Abhishekundalia <abhishekundalia666@gmail.com>
2016-12-18 01:36:47 +0530

    Implemented Complete functionality TODO- UI convert to list view

--- RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/MainActivity.java
+++ RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/MainActivity.java
@@ -6,13 +6,19 @@ import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.List;
+
+import eu.chainfire.libsuperuser.Shell;
+
 public class MainActivity extends AppCompatActivity {
 
     Button reboot, shutdown, ping;
+    boolean suAvailable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +30,63 @@ public class MainActivity extends AppCompatActivity {
         shutdown = (Button) findViewById(R.id.shutdown);
         ping = (Button) findViewById(R.id.ping);
 
-        //Assigning listener to each Button
-        reboot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new RootBoot(v.getContext()).execute("reboot");
-            }
-        });
+        suAvailable = Shell.SU.available();
+        if (suAvailable) {
+            //Assigning listener to each Button
+            reboot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(view.getContext())
+                            .setMessage("Are you sure you want to " + getText(R.string.reboot) + " ?")
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.reboot, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new RootBoot(MainActivity.this).execute("reboot");
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+            });
 
-        shutdown.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new RootBoot(v.getContext()).execute("shutdown");
-            }
-        });
+            shutdown.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(view.getContext())
+                            .setMessage("Are you sure you want to " + getText(R.string.shutdown) + " ?")
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.shutdown, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new RootBoot(MainActivity.this).execute("shutdown");
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+            });
 
-        ping.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new AlertDialog.Builder(view.getContext())
-                        .setMessage("Are you sure you want to " + getText(R.string.ping) + " ?")
-                        .setCancelable(false)
-                        .setPositiveButton(R.string.ping, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                new RootBoot(MainActivity.this).execute("ping");
-                            }
-                        })
-                        .setNegativeButton("Cancel", null)
-                        .show();
-            }
-        });
+            ping.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(view.getContext())
+                            .setMessage("Are you sure you want to " + getText(R.string.ping) + " ?")
+                            .setCancelable(false)
+                            .setPositiveButton(R.string.ping, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new RootBoot(MainActivity.this).execute("ping");
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.this, "Phone not Rooted!", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     public class RootBoot extends AsyncTask<String, Void, String> {
@@ -71,15 +103,41 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String params) {
-            Toast.makeText(context, params, Toast.LENGTH_LONG).show();
-
+            if (params == null) {
+                Toast.makeText(context, "Null returned", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(context, params, Toast.LENGTH_LONG).show();
+            }
         }
 
         @Override
         protected String doInBackground(String... params) {
+            String result = null;
+            switch (params[0]) {
+                case "reboot":
+                    Shell.SU.run("reboot");
+                    result = null;
+                    break;
 
+                case "shutdown":
+                    Shell.SU.run("reboot -p");
+                    result = null;
+                    break;
 
-            return params[0];
+                case "ping":
+                    List<String> suResult;
+
+                    if (suAvailable) {
+                        suResult = Shell.SU.run(new String[]{"su; "});
+                        Log.i("sysUI: ", suResult.toString());
+                        result = suResult.toString();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return result;
         }
     }
 }
