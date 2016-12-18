06e2fc681c7c825dc0b1e891cc37130b4af39f16
Abhishekundalia <abhishekundalia666@gmail.com>
2016-12-19 04:22:15 +0530

    implemented Adapter - functionality working fine with ListView TODO- Change App Icon change app color

--- RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/ListViewAdapter.java
+++ RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/ListViewAdapter.java
@@ -1,10 +1,12 @@
 package com.awiserk.kundalias.rootboot;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,12 +16,36 @@ import java.util.ArrayList;
 
 public class ListViewAdapter extends ArrayAdapter<ButtonData> {
     public ListViewAdapter(Context context, ArrayList<ButtonData> buttonDataArrayList) {
-        super(context, 0, buttonDataArrayList);
+        super(context, R.layout.list_item, buttonDataArrayList);
     }
 
-    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        //Check if the existing view is being reused, else inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        //Get the current button data
+        ButtonData currentButtonData = getItem(position);
+
+        //Find the ImageView from list_item.xml and assign current button data to it
+        ImageView buttonImage = (ImageView) listItemView.findViewById(R.id.image_icon);
+        buttonImage.setImageResource(currentButtonData.getImageResourceID());
+
+        //Find the Title View from listitem.xml and assign the current button data title to it
+        TextView title = (TextView) listItemView.findViewById(R.id.title);
+        title.setText(currentButtonData.getTitleID());
+
+        //Find the Description View from the listitem.xml and assign the current button data desc to it
+        TextView description = (TextView) listItemView.findViewById(R.id.description);
+        description.setText(currentButtonData.getDescriptionID());
+
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
     }
 }
--- RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/MainActivity.java
+++ RootBoot/app/src/main/java/com/awiserk/kundalias/rootboot/MainActivity.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,26 +23,77 @@ public class MainActivity extends AppCompatActivity {
     Button reboot, shutdown, rebootRecovery;
     boolean suAvailable = false;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.button_list);
 
-        //assing Buttons from XML
+      /*  //assing Buttons from XML
         reboot = (Button) findViewById(R.id.reboot);
         shutdown = (Button) findViewById(R.id.shutdown);
         rebootRecovery = (Button) findViewById(R.id.rebootrecovery);
-
+*/
         // Add data to a list
-        ArrayList<ButtonData> buttonDatas = new ArrayList<ButtonData>();
-        buttonDatas.add(new ButtonData(R.drawable.ic_power_settings_new_black_24dp, R.string.shutdown, R.string.shutdowndesc));
-        buttonDatas.add(new ButtonData(R.drawable.ic_rotate_right_black_24dp, R.string.reboot, R.string.rebootdesc));
-        buttonDatas.add(new ButtonData(R.drawable.ic_sync_problem_black_24dp, R.string.rebootrecovery, R.string.rebootrecdesc));
+        final ArrayList<ButtonData> buttonDatas = new ArrayList<ButtonData>();
+        buttonDatas.add(new ButtonData(R.drawable.ic_power_settings_new_black_48dp, R.string.shutdown, R.string.shutdowndesc));
+        buttonDatas.add(new ButtonData(R.drawable.ic_rotate_right_black_48dp, R.string.reboot, R.string.rebootdesc));
+        buttonDatas.add(new ButtonData(R.drawable.ic_sync_problem_black_48dp, R.string.rebootrecovery, R.string.rebootrecdesc));
 
+        //Call listViewAdapter and set values of the arraylist in the list view
+        ListViewAdapter listViewAdapter = new ListViewAdapter(this, buttonDatas);
+
+        //Create a Listview object pointing to Button_List.xml
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //Set adapter to the listview
+        listView.setAdapter(listViewAdapter);
+
+        //Check if Root access is available or not
         suAvailable = Shell.SU.available();
         if (suAvailable) {
             //Assigning listener to each Button
-            reboot.setOnClickListener(new View.OnClickListener() {
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.i("Clicked", " button clicked");
+                    final String itemTitle = getText(buttonDatas.get(position).getTitleID()).toString();
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("Are you sure you want to " + itemTitle + " ?")
+                            .setCancelable(false)
+                            .setPositiveButton(itemTitle, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    new RootBoot(MainActivity.this).execute(itemTitle);
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .show();
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.this, "Phone not Rooted!", Toast.LENGTH_SHORT).show();
+            //Alert user with dialog to provide permission or quit app
+            new AlertDialog.Builder(this)
+                    .setMessage("Your application does not have root access. So now the application will Quit!").setCancelable(false)
+                    .setPositiveButton("Quit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finish();
+                        }
+                    }).setNegativeButton("Restart App", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    MainActivity.this.finish();
+                    Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            }).show();
+        }
+    }
+
+           /* listView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     new AlertDialog.Builder(view.getContext())
@@ -88,29 +142,10 @@ public class MainActivity extends AppCompatActivity {
                             .setNegativeButton("Cancel", null)
                             .show();
                 }
-            });
-        } else {
-            Toast.makeText(MainActivity.this, "Phone not Rooted!", Toast.LENGTH_SHORT).show();
-            new AlertDialog.Builder(this)
-                    .setMessage("Your application does not have root access. So now the application will Quit!").setCancelable(false)
-                    .setPositiveButton("Quit", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            MainActivity.this.finish();
-                        }
-                    }).setNegativeButton("Restart App", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    MainActivity.this.finish();
-                    Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(i);
-                }
-            }).show();
-        }
+            });*/
 
-    }
 
+    //Async task for executing operation in background thread
     public class RootBoot extends AsyncTask<String, Void, Void> {
         Context context = null;
 
@@ -132,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(String... params) {
             String result = null;
             switch (params[0]) {
-                case "reboot":
+                case "Reboot Phone":
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -142,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
                     Shell.SU.run("sleep 2 && reboot");
                     break;
 
-                case "shutdown":
+                case "Shutdown Phone":
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -152,7 +187,7 @@ public class MainActivity extends AppCompatActivity {
                     Shell.SU.run("sleep 2 && reboot -p");
                     break;
 
-                case "rebootrecovery":
+                case "Reboot Recovery":
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
